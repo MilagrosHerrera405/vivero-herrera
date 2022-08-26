9933e42bb7a528c9ecf311d10c4e50c7560b89a4
Milagros Herrera <110346247+MilagrosHerrera405@users.noreply.github.com>
2022-08-26 01:46:32 -0300

    deco carrito

--- src/components/ItemListContainer/ItemListContainer.elements.js
+++ src/components/ItemListContainer/ItemListContainer.elements.js
@@ -3,8 +3,8 @@ import styled from "styled-components";
 export const Container = styled.div``;
 
 export const Catalogo = styled.div`
-  color: #ac4425;
-  display: flex;
+color: #ac4425;
+display: flex;
 flex-direction: row;
 flex-wrap: nowrap;
 justify-content:center;
