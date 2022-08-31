3f7f999270c2484d12194b1ad4b56a7fd1330021
Milagros Herrera <110346247+MilagrosHerrera405@users.noreply.github.com>
2022-08-31 16:06:02 -0300

    commit

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -6,11 +6,9 @@ const Item = ({ info }) => {
   return (
     <div className="film">
       <div className="card text-center">
-      <p className="title">{info.title}</p>
+        <p className="title">{info.title}</p>
         <img className="card-img-top" src={info.image} alt="" />
-
         <div className="card-body text light">
-          
           <a href={"#"} className="btn btn-outline-success">
             Ver detalle del producto
           </a>
